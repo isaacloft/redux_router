3f8f2515c4e2cdb8df230a8f5d2b29b901c6061f
isaacloft <isaacloft0906@gmail.com>
2018-09-29 15:12:25 +1000

    added switch for routing loose match

---
+++ src/components/posts_new.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class PostsNew extends React.Component {
+	
+
+	// constructor(props) {
+	// 	super(props);
+	// }
+
+	render() {
+		return (
+			<div>new</div>
+		);
+	}
+}
